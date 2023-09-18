52c75d2c6724de064df4b2ac61be896a0a2f7bed
YUN DAEWON <131121496+YUNDAEWON1@users.noreply.github.com>
2023-09-18 15:32:56 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = 0;
+    
+    if(price<100000)
+        answer=price;
+   if(price>=100000)
+       answer=price*0.95;
+        
+    if(price>=300000)
+        answer=price*0.90;
+       
+    if(price>=500000)
+        answer=price*0.80;
+       
+        
+    return answer;
+}

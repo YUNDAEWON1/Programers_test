a89e68b12defe1667645986a951bdb55d58666b2
YUN DAEWON <131121496+YUNDAEWON1@users.noreply.github.com>
2023-09-18 15:27:51 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 1;
+    if(num1==num2)
+    {
+    return answer;
+    }
+    else
+    {
+        return -1;
+    }
+}
